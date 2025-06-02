0e0f33fbf67ed90cf324675dd0c5459901e86522
Amal Pradeep <amalpradeep12@gmail.com>
2025-06-02 09:31:58 +0530

    add analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,32 +1,32 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
-
 export const metadata = {
-  title: "Foodtrack",
-  description: "Track your meals!",
+  title: 'Foodtrack',
+  description: 'Track your meals!',
 };
-
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel='icon' href='/logo.png' sizes='any' type='png' />
+        <link rel="icon" href="/logo.png" sizes="any" type="png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         {children}
       </body>
     </html>
